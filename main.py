1b3165d95ad056ffe4f1e94a515ce32586d74af0
xReniar <ilaorainer@gmail.com>
2024-10-20 18:40:18 +0200

    Read json directly instead of passing arguments

--- main.py
+++ main.py
@@ -1,20 +1,9 @@
-import argparse
+import json
+
 
 def main(args):
     print(args)
 
-
-def parse_args():
-    parser = argparse.ArgumentParser(
-        description="Script for generating training data for Text Detection and Text Recognition",
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-    )
-
-    parser.add_argument("--config", type=str, required=True, help="path to config.json")
-    args = parser.parse_args()
-    
-    return args
-
 if __name__ == "__main__":
-    args = parse_args()
-    main(args)
+    config = json.load(open("config/config.json"))
+    
