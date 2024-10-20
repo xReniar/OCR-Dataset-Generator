ad047ef36ce06a3cfbdf2d30b9b477f3764f36e4
xReniar <ilaorainer@gmail.com>
2024-10-20 17:30:54 +0200

    Pass only the path to config file

--- main.py
+++ main.py
@@ -10,14 +10,9 @@ def parse_args():
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
 
-    parser.add_argument("--task", type=str, required=True, help="task used to generate the traning data")
-    parser.add_argument("--ocr", type=str, required=True, help="OCR system that will be used for training")
-    parser.add_argument("--datasets", type=str, nargs="+", required=True, help="datasets that are needed to generate the traning data")
-    parser.add_argument("--name", type=str, required=True, help="name of the folder where the traning data will be stored")
+    parser.add_argument("--config", type=str, required=True, help="path to config.json")
     args = parser.parse_args()
-
-    # convert each dataset in the list to lowercase
-    args.datasets = [dataset.lower() for dataset in args.datasets]
+    
     return args
 
 if __name__ == "__main__":
