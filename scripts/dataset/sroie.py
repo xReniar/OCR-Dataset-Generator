a22d959922db1d72ca8e565744ba44d2f75d2f92
xReniar <ilaorainer@gmail.com>
2025-01-18 11:02:39 +0100

    Download annotations for sroie in the specified format

--- scripts/dataset/sroie.py
+++ scripts/dataset/sroie.py
@@ -1,4 +1,5 @@
 from dataset import Dataset
+from datasets import load_dataset
 
 
 class SROIE(Dataset):
@@ -6,12 +7,24 @@ class SROIE(Dataset):
         super().__init__(
             local_datasets = [],
             online_datasets = {
-                "sroie": ""
+                "sroie": "darentang/sroie"
             }
         )
 
-    def download_images(self, dataset:str | None = None):
-        super().download_images(dataset)
-        link:str = self.online_datasets[dataset if dataset != None else self.get_class_name()]
+    def download_data(self, dataset:str | None = None) -> None:
+        super().download_data(dataset)
 
-        # to do
+        for split in ["train", "test"]:
+            for sample in load_dataset(self._current_link, split=split):
+                words = sample["words"]
+                bboxes = sample["bboxes"]
+                #ner_tags = sample["ner_tags"]
+                image_path:str = sample["image_path"]
+
+                image_name = image_path.split("/")[-1]
+                file_name = image_name.replace(".jpg", ".txt")
+
+                file = open(f"{self.get_path(dataset)}/{split}/{file_name}","w")
+                for word, bbox in zip(words, bboxes):
+                    file.write(f"{word}\t{bbox}\n")
+                file.close()
