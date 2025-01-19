ded4bc53affab8f34d56ab9f9eb4bc8865d0a853
xReniar <ilaorainer@gmail.com>
2025-01-20 00:31:51 +0100

    Add generator class for creating training data

--- scripts/generator/generator.py
+++ scripts/generator/generator.py
@@ -2,5 +2,23 @@ from abc import ABC, abstractmethod
 
 
 class Generator(ABC):
-    def __init__(self):
+    def __init__(self,
+        datasets: tuple,
+        task: str,
+        output_name: str,
+        transforms,
+    ) -> None:
         super().__init__()
+
+        self.datasets: tuple = datasets
+        self.task: str = task
+        self.output_name: str = output_name
+        self.transforms = transforms
+
+    @abstractmethod
+    def det_generator(self, dataset:str) -> None:
+        pass
+    
+    @abstractmethod
+    def rec_generator(self, dataset:str) -> None:
+        pass
