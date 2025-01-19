533e1e15e97ed204ae1fb4e59803867a9cfe17e4
xReniar <ilaorainer@gmail.com>
2025-01-20 00:32:21 +0100

    Add doctr generator

--- scripts/generator/doctr.py
+++ scripts/generator/doctr.py
@@ -1,6 +1,19 @@
 from generator import Generator
 
 
-class DocTRGenerator(Generator):
-    def __init__(self):
-        super().__init__()
+class DoctrGenerator(Generator):
+    def __init__(self,
+        datasets: tuple,
+        task: str,
+        output_name: str
+    ) -> None:
+        super().__init__(
+
+        )
+
+    def det_generator(self, dataset:str) -> None:
+        return super().det_generator()
+    
+    def rec_generator(self, dataset:str) -> None:
+        return super().rec_generator()
+    
