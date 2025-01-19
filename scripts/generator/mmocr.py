dbeab43b223c2d514ad89665e2a4ee365ad8c643
xReniar <ilaorainer@gmail.com>
2025-01-20 00:32:32 +0100

    Add mmocr generator

--- scripts/generator/mmocr.py
+++ scripts/generator/mmocr.py
@@ -2,5 +2,17 @@ from generator import Generator
 
 
 class MMOCRGenerator(Generator):
-    def __init__(self):
-        super().__init__()
+    def __init__(self,
+        datasets: tuple,
+        task: str,
+        output_name: str
+    ) -> None:
+        super().__init__(
+            
+        )
+
+    def det_generator(self, dataset:str) -> None:
+        return super().det_generator()
+    
+    def rec_generator(self, dataset:str) -> None:
+        return super().rec_generator()
