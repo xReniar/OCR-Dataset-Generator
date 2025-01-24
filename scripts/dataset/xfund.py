40f30516886bb6862b18f817cd2def930d53de4c
xReniar <ilaorainer@gmail.com>
2025-01-24 16:08:19 +0100

    Update xfund

--- scripts/dataset/xfund.py
+++ scripts/dataset/xfund.py
@@ -3,30 +3,33 @@ import requests
 import zipfile
 import os
 
-URL = "https://github.com/doc-analysis/XFUND/releases/download/v1.0"
+ROOT = "https://github.com/doc-analysis/XFUND/releases/download/v1.0"
+
+CONFIG = {
+    "xfund-de": f"{ROOT}/de.",
+    "xfund-es": f"{ROOT}/es.",
+    "xfund-fr": f"{ROOT}/fr.",
+    "xfund-it": f"{ROOT}/it.",
+    "xfund-ja": f"{ROOT}/ja.",
+    "xfund-pt": f"{ROOT}/pt.",
+    "xfund-zh": f"{ROOT}/zh."
+}
+
 
 class XFUND(Dataset):
-    def __init__(self):
-        super().__init__(
-            local_datasets = [],
-            online_datasets = {
-                "de": f"{URL}/de.",
-                "es": f"{URL}/es.",
-                "fr": f"{URL}/fr.",
-                "it": f"{URL}/it.",
-                "ja": f"{URL}/ja.",
-                "pt": f"{URL}/pt.",
-                "zh": f"{URL}/zh.",
-            }
-        )
+    def __init__(
+        self,
+        config: dict
+    ) -> None:
+        super().__init__(config)
 
-    def download_data(self, dataset:str | None = None) -> None:
-        super().download_data(dataset)
+    def _download(self):
+        super()._download()
 
         # download and extraction of annotations
         for split in ["train", "val"]:
             folder = "train" if split == "train" else "test"
-            annotation = requests.get(f"{self._current_link}{split}.json", allow_redirects=True).json()
+            annotation = requests.get(f"{CONFIG[self._current]}{split}.json", allow_redirects=True).json()
             documents:list = annotation["documents"]
             
             for element in documents:
@@ -35,7 +38,7 @@ class XFUND(Dataset):
                 # contains 'fname', 'width' and 'height'
                 fname:str = element["img"]["fname"]
 
-                file = open(f"{self.get_path(dataset)}/{folder}/{fname.split('.')[0]}.txt", "w")
+                file = open(f"{self.path()}/{folder}/{fname.split('.')[0]}.txt", "w")
                 for labels in document:
                     for word in labels["words"]:
                         file.write(f"{word['text']}\t{word['box']}\n")
@@ -44,13 +47,13 @@ class XFUND(Dataset):
         # download images
         for split in ["train", "val"]:
             folder = "train" if split == "train" else "test"
-            response = requests.get(f"{self._current_link}{split}.zip")
-            zip_fn = self._current_link.split("/")[-1]
-            current_path = f"{self.get_path(dataset)}/{zip_fn}{split}.zip"
+            response = requests.get(f"{CONFIG[self._current]}{split}.zip")
+            zip_fn = CONFIG[self._current].split("/")[-1]
+            current_path = f"{self.path()}/{zip_fn}{split}.zip"
             with open(current_path,"wb") as f:
                 f.write(response.content)
 
             with zipfile.ZipFile(current_path, "r") as zip_ref:
-                zip_ref.extractall(os.path.join(self.get_path(dataset), "images"))
+                zip_ref.extractall(os.path.join(self.path(), "images"))
 
             os.remove(current_path)
