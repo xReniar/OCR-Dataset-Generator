fc04fae63ae2afb230f657b02dbfa3744473e2cf
xReniar <ilaorainer@gmail.com>
2025-01-24 20:10:24 +0100

    Create loader function for detection and recognition

--- scripts/dataset/dataset.py
+++ scripts/dataset/dataset.py
@@ -51,12 +51,33 @@ class Dataset(ABC):
 
     def load_data(
         self,
-        task: str
+        task: str,
+        split: str
     ) -> None:
-        pass
+        assert split in ["train", "test"], f"No split of type: {split}"
+
+        if task == "det":
+            return self.__det_loader(split)
+        if task == "rec":
+            return self.__rec_loader(split)
+
+    def __det_loader(self, split:str):
+        base_path = f"{self.path()}/{split}"
+
+        loader = []
+        for file in os.listdir(base_path):
+            labels = open(f"{base_path}/{file}","r")
+            for label in labels.readlines():
+                row = label.replace("\n","").split("\t")
+                print(row)
+            labels.close()
+
+    def __rec_loader(self, split:str):
+        for file in os.listdir(f"{self.path()}/{split}"):
+            pass
 
     @abstractmethod
-    def _download(
+    def download(
         self
     ) -> None:
         '''
