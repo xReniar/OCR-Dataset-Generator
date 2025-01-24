ddc6475550fad01007bd23fbb6c4acf5ea9fe059
xReniar <ilaorainer@gmail.com>
2025-01-24 16:07:55 +0100

    Update funsd

--- scripts/dataset/funsd.py
+++ scripts/dataset/funsd.py
@@ -1,38 +1,40 @@
 from dataset import Dataset
 import requests
 import zipfile
-import os
 import shutil
 import json
+import os
 
-# implement something that ignores some characters
+
+CONFIG = {
+    "funsd": "https://guillaumejaume.github.io/FUNSD/dataset.zip"
+}
+
 class FUNSD(Dataset):
-    def __init__(self):
-        super().__init__(
-            local_datasets = [],
-            online_datasets = {
-                "funsd": "https://guillaumejaume.github.io/FUNSD/dataset.zip"
-            }
-        )
+    def __init__(
+        self,
+        config: dict
+    ) -> None:
+        super().__init__(config)
 
-    def download_data(self, dataset:str | None = None) -> None:
-        super().download_data(dataset)
+    def _download(self):
+        super()._download()
 
-        response = requests.get(self._current_link)
-        zip_fn = self._current_link.split("/")[-1]
-        with open(f"{self.get_path(dataset)}/{zip_fn}","wb") as f:
+        response = requests.get(CONFIG[self._current])
+        zip_fn = CONFIG[self._current].split("/")[-1]
+        with open(f"{self.path()}/{zip_fn}","wb") as f:
             f.write(response.content)
 
-        with zipfile.ZipFile(f"{self.get_path(dataset)}/{zip_fn}", "r") as zip_ref:
-            zip_ref.extractall(self.get_path(dataset))
+        with zipfile.ZipFile(f"{self.path()}/{zip_fn}", "r") as zip_ref:
+            zip_ref.extractall(self.path())
 
-        shutil.rmtree(os.path.join(self.get_path(dataset), "__MACOSX"))
-        os.remove(os.path.join(self.get_path(dataset), zip_fn))
+        shutil.rmtree(os.path.join(self.path(), "__MACOSX"))
+        os.remove(os.path.join(self.path(), zip_fn))
 
         # move images inside 'images' folder
         for split in ["train", "test"]:
-            src_folder = f"{self.get_path(dataset)}/dataset/{split}ing_data/images"
-            dst_folder = f"{self.get_path(dataset)}/images"
+            src_folder = f"{self.path()}/dataset/{split}ing_data/images"
+            dst_folder = f"{self.path()}/images"
 
             for item in os.listdir(src_folder):
                 shutil.move(
@@ -42,11 +44,11 @@ class FUNSD(Dataset):
 
         # creating annotations
         for split in ["train", "test"]:
-            src_folder = f"{self.get_path(dataset)}/dataset/{split}ing_data/annotations"
-            dst_folder = f"{self.get_path(dataset)}/{split}"
+            src_folder = f"{self.path()}/dataset/{split}ing_data/annotations"
+            dst_folder = f"{self.path()}/{split}"
 
             for annotation_file in os.listdir(src_folder):
-                file = open(f"{self.get_path(dataset)}/{split}/{annotation_file.split('.')[0]}.txt","w")
+                file = open(f"{self.path()}/{split}/{annotation_file.split('.')[0]}.txt","w")
                 annotation:list = json.load(open(f"{src_folder}/{annotation_file}", "r"))["form"]
                 
                 for label in annotation:
@@ -55,4 +57,4 @@ class FUNSD(Dataset):
 
                 file.close()
 
-        shutil.rmtree(os.path.join(self.get_path(dataset), "dataset"))
+        shutil.rmtree(os.path.join(self.path(), "dataset"))
