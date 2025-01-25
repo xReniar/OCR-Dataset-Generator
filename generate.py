7ca18ba053878fdac90bca241a019d8f7e56c6e6
xReniar <ilaorainer@gmail.com>
2025-01-25 17:00:28 +0100

    Implement download step for dataset

--- generate.py
+++ generate.py
@@ -1,32 +1,78 @@
+from scripts.dataset import *
 import json
-import argparse
 
+__OCR_SYSTEMS__ = ["doctr", "mmocr", "paddleocr"]
 
-def main(args):
-    print(args)
+def download_data(
+    requested_datasets: list[Dataset],
+    sub_datasets: dict
+) -> None:
+    for dataset in requested_datasets:
+        if dataset.has_variants():
+            for sub in sub_datasets[dataset._root_name()]:
+                dataset.set_to(sub)
+                if not(dataset.is_downloaded()):
+                    dataset.download()
+        else:
+            if not(dataset.is_downloaded()):
+                    dataset.download()
 
-def parse_args():
-    config:dict = json.load(open("config/config.json", "r"))
-    DATASETS = config["datasets"].keys()
-    OCR_SYSTEMS = config["ocr-system"]
+def main(
+    test_name: str, 
+    ocr_system: str,
+    task: list,
+    datasets
+) -> None:
+    # get root datasets (without the '-' after every name)
+    main_datasets = set()
+    sub_datasets = dict()
+    for dataset in datasets:
+        if isinstance(dataset, str):
+            main_datasets.add(dataset)
+        if isinstance(dataset, list):
+            root, _ = dataset[0].split("-")
+            sub_datasets[root] = dataset
+            main_datasets.add(root)
+    main_datasets: list[str] = list(main_datasets)
 
-    parser = argparse.ArgumentParser(
-        description="Training data script generator for text detection and text recognition",
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter
-    )
+    # generating instances of datasets (Dataset object)
+    requested_datasets:list[Dataset] = []
+    for dataset in main_datasets:
+        name_upper = dataset.upper()
+        requested_datasets.append(DATASETS[name_upper](CONFIG[name_upper]))
 
-    parser.add_argument("--name", type=str, required=True, default="custom_data", help="name of the training data")
-    parser.add_argument("--task", type=str, required=True, choices=["det", "rec"], help="task for the training data")
-    parser.add_argument("--ocr-system", type=str, required=True, choices=OCR_SYSTEMS)
-    parser.add_argument("--datasets", type=str, required=True, default=DATASETS, help="datasets that are going to be used to generate the training data")
-    parser.add_argument("--blur", dest="blur", action="store_true", help="add blurring to training data")
-    parser.add_argument("--skew", dest="skew", action="store_true", help="add skewing to training data")
-    parser.add_argument("--distort", dest="distort", action="store_true", help="add distortion to training data")
+    # download data if necessary
+    download_data(requested_datasets, sub_datasets)
 
-    args = parser.parse_args()
-    return args
 
 if __name__ == "__main__":
-    args = parse_args()
-    main(args)
+    config:dict = json.load(open("config/config.json", "r"))
+    datasets:dict = config["datasets"]
+
+    defined_classes = list(map(lambda x: str(x).lower(), list(DATASETS.keys())))
+    config_classes = list(datasets.keys())
+    if len(defined_classes) > len(config_classes):
+        raise BaseException("There are some datasets that are not in the `config.json`")
+    if len(defined_classes) < len(config_classes):
+        raise BaseException("Some datasets in the `config.json` do not have a script in `./scripts/dataset/`")
+
+    selected = []
+    for dataset in list(datasets.keys()):
+        value = datasets[dataset]
+        if isinstance(value, str):
+            selected.append(dataset) if value == "y" else None
+        elif isinstance(value, dict):
+            sub_list = []
+            for sub in value.keys():
+                sub_list.append(sub) if value[sub] == "y" else None
+            selected.append(sub_list) if len(sub_list) > 0 else None
+        else:
+            raise ValueError(f"Type '{type(value)}' not available")
+        
+    main(
+        config["test-name"],
+        config["ocr-system"],
+        config["task"],
+        selected
+    )
     
