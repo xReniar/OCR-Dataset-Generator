00fa42cd7cb5f439b284e5cc8cd4c269c24d4b4a
xReniar <ilaorainer@gmail.com>
2025-01-26 17:52:45 +0100

    Add extension_map to track file type

--- scripts/generator/doctr.py
+++ scripts/generator/doctr.py
@@ -28,27 +28,36 @@ class DoctrGenerator(Generator):
                 imgs_dir = os.listdir(f"{current_path}/images")
                 imgs_dir.sort()
 
+                extension_map = {}
+                for img in imgs_dir:
+                    name, _ = img.split(".")
+                    extension_map[f"{name}.txt"] = img
+
                 os.makedirs(f"../../output/doctr-det/{split}/images", exist_ok=True)
                 label_dir = os.listdir(f"{current_path}/{split}")
                 label_dir.sort()
 
                 # images creations
-                self.copy_file(label_dir, imgs_dir, current_path, split)
+                self.copy_file(
+                    label_dir,
+                    extension_map,
+                    current_path,
+                    f"../../output/{self.name()}-det/{split}/images",
+                )
 
                 # labels creation
                 for label in label_dir:
-                    img_name = label.replace("txt","")
-                    extension = [f for f in imgs_dir if f.startswith(img_name)][0].split(".")[1]
+                    img_name = extension_map[label]
 
                     # creating label instance for labels.json
-                    img = Image.open(f"{current_path}/images/{img_name}{extension}")
+                    img = Image.open(f"{current_path}/images/{img_name}")
                     polygons = []
                     text_file = open(f"{current_path}/{split}/{label}")
                     for (_, bbox) in self.read_rows(text_file):
                         x1, y1, x2, y2 = bbox
                         polygons.append([[x1, y1],[x2, y1],[x2, y2],[x1, y2]])
                     text_file.close()
-                    labels[f"{img_name}{extension}"] = dict(
+                    labels[f"{img_name}"] = dict(
                         img_dimensions = (img.width, img.height),
                         img_hash = hashlib.sha256(img.tobytes()).hexdigest(),
                         polygons = polygons
