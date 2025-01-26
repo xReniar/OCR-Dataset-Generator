e27e0169b13c21d7f160cea5d0fe45bc4577dcfc
xReniar <ilaorainer@gmail.com>
2025-01-26 01:31:33 +0100

    Implement det data generation for doctr

--- scripts/generator/doctr.py
+++ scripts/generator/doctr.py
@@ -1,4 +1,8 @@
 from generator import Generator
+from PIL import Image
+import hashlib
+import json
+import os
 
 
 class DoctrGenerator(Generator):
@@ -15,13 +19,60 @@ class DoctrGenerator(Generator):
     def generate_det_data(self):
         super().generate_det_data()
 
-        for dataset in self.datasets:
-            print(dataset)
+        labels = {}
+
+        for split in ["train", "test"]:
+            for dataset in self.datasets:
+                current_path = f"../../data/{dataset}"
+
+                imgs_dir = os.listdir(f"{current_path}/images")
+                imgs_dir.sort()
+
+                os.makedirs(f"../../output/doctr-det/{split}/images", exist_ok=True)
+                label_dir = os.listdir(f"{current_path}/{split}")
+                label_dir.sort()
+
+                # images creations
+                self.copy_file(label_dir, imgs_dir, current_path, split)
+
+                # labels creation
+                for label in label_dir:
+                    img_name = label.replace("txt","")
+                    extension = [f for f in imgs_dir if f.startswith(img_name)][0].split(".")[1]
+
+                    # creating label instance for labels.json
+                    img = Image.open(f"{current_path}/images/{img_name}{extension}")
+                    polygons = []
+                    text_file = open(f"{current_path}/{split}/{label}")
+                    for (_, bbox) in self.read_rows(text_file):
+                        x1, y1, x2, y2 = bbox
+                        polygons.append([[x1, y1],[x2, y1],[x2, y2],[x1, y2]])
+                    text_file.close()
+                    labels[f"{img_name}{extension}"] = dict(
+                        img_dimensions = (img.width, img.height),
+                        img_hash = hashlib.sha256(img.tobytes()).hexdigest(),
+                        polygons = polygons
+                    )
+                    img.close()
+            with open(f"../../output/doctr-det/{split}/labels.json","w") as file:
+                json.dump(labels,file, indent=4)
+
 
     def generate_rec_data(self):
-        super().generate_rec_data()    
+        super().generate_rec_data()
+
+        for split in ["train", "test"]:
+            for dataset in self.datasets:
+                current_path = f"../../data/{dataset}"
+
+                imgs_dir = os.listdir(f"{current_path}/images")
+                imgs_dir.sort()
+
+                os.makedirs(f"../../output/doctr-rec/{split}/images", exist_ok=True)
+                label_dir = os.listdir(f"{current_path}/{split}")
+                label_dir.sort()
 
 
-x = DoctrGenerator(["funsd", "xfund-de"])
+x = DoctrGenerator(["funsd","xfund-de"])
 
 x.generate_det_data()
