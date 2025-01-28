adbd881a195799216e0fcaacc339ee34301454fb
xReniar <ilaorainer@gmail.com>
2025-01-28 15:30:16 +0100

    Update doctr generator when reading rows of current label file

--- scripts/generator/doctr.py
+++ scripts/generator/doctr.py
@@ -46,11 +46,9 @@ class DoctrGenerator(Generator):
                     # creating label instance for labels.json
                     img = Image.open(f"{current_path}/images/{img_name}")
                     polygons = []
-                    text_file = open(f"{current_path}/{split}/{label}")
-                    for (_, bbox) in self.read_rows(text_file):
+                    for (_, bbox) in self.read_rows(f"{current_path}/{split}/{label}"):
                         x1, y1, x2, y2 = bbox
                         polygons.append([[x1, y1],[x2, y1],[x2, y2],[x1, y2]])
-                    text_file.close()
                     labels[f"{img_name}"] = dict(
                         img_dimensions = (img.width, img.height),
                         img_hash = hashlib.sha256(img.tobytes()).hexdigest(),
@@ -74,14 +72,12 @@ class DoctrGenerator(Generator):
                 extension_map = self.extension_map(imgs_dir)
 
                 for label in sorted(os.listdir(f"{current_path}/{split}")):
-                    text_file = open(f"{current_path}/{split}/{label}")
                     img = Image.open(f"{current_path}/images/{extension_map[label]}")
-                    for index, (text, bbox) in enumerate(self.read_rows(text_file)):
+                    for index, (text, bbox) in enumerate(self.read_rows(f"{current_path}/{split}/{label}")):
                         crop_name = extension_map[label].replace(".",f"-{index}.")
                         img.crop(bbox).save(f"{self._root_path}/{split}/images/{crop_name}")
                         labels[crop_name] = text
                     img.close()
-                    text_file.close()
 
             with open(f"{self._root_path}/{split}/labels.json","w") as file:
                 json.dump(labels,file, indent=4)
