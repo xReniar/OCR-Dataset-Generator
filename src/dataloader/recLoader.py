3470bd9f142e2b6931f1a1ce034e8fdebabf408c
xReniar <ilaorainer@gmail.com>
2025-03-04 00:04:00 +0100

    Implement data loading for text recognition task

--- src/dataloader/recLoader.py
+++ src/dataloader/recLoader.py
@@ -1,4 +1,5 @@
 from .dataloader import Dataloader
+import os
 
 class RecDataloader(Dataloader):
     def __init__(
@@ -11,5 +12,19 @@ class RecDataloader(Dataloader):
             datasets
         )
 
-    def load_data(self, split:str) -> None:
-        pass
+    def load_data(self) -> None:
+        for dataset in sorted(self.datasets):
+            self.data[dataset] = dict(
+                train=[],
+                test=[]
+            )
+            for split in ["train", "test"]:
+                curr_list = []
+                split_folder_path = f"./data/{dataset}/{split}"
+                for label_fn in sorted(os.listdir(split_folder_path)):
+                    img_fn, img_type, _ = tuple(label_fn.split("."))
+                    for i, (text, bbox) in enumerate(self.read_label(f"{split_folder_path}/{label_fn}")): 
+                        curr_list.append({
+                            f"{img_fn}-{i}.{img_type}": [bbox,text]
+                        })
+                self.data[dataset][split] = curr_list
