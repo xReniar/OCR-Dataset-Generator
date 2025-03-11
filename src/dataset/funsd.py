480bbaa03e85d1ef546065f1bd031bf9324668e6
xReniar <ilaorainer@gmail.com>
2025-03-11 15:57:15 +0100

    Replaced pillow with opencv

--- src/dataset/funsd.py
+++ src/dataset/funsd.py
@@ -17,24 +17,23 @@ class FUNSD(Dataset):
     ) -> None:
         super().__init__(config)
 
-    def download(self):
-        super().download()
-
+    def _download(self) -> None:
         response = requests.get(self.config[self._current])
         zip_fn = self.config[self._current].split("/")[-1]
         with open(f"{self.path()}/{zip_fn}","wb") as f:
             f.write(response.content)
 
-        with zipfile.ZipFile(f"{self.path()}/{zip_fn}", "r") as zip_ref:
+        zip_path = os.path.join(self.path(), zip_fn)
+        with zipfile.ZipFile(zip_path, "r") as zip_ref:
             zip_ref.extractall(self.path())
 
         shutil.rmtree(os.path.join(self.path(), "__MACOSX"))
-        os.remove(os.path.join(self.path(), zip_fn))
+        os.remove(zip_path)
 
         # move images inside 'images' folder
         for split in ["train", "test"]:
-            src_folder = f"{self.path()}/dataset/{split}ing_data/images"
-            dst_folder = f"{self.path()}/images"
+            src_folder = os.path.join(self.path(), "dataset", f"{split}ing_data", "images")
+            dst_folder = os.path.join(self.path(), split, "images")
 
             for item in os.listdir(src_folder):
                 shutil.move(
@@ -44,12 +43,13 @@ class FUNSD(Dataset):
 
         # creating annotations
         for split in ["train", "test"]:
-            src_folder = f"{self.path()}/dataset/{split}ing_data/annotations"
-            dst_folder = f"{self.path()}/{split}"
+            src_folder = os.path.join(self.path(), "dataset", f"{split}ing_data", "annotations")
+            dst_folder = os.path.join(self.path(), split, "labels")
 
             for annotation_file in os.listdir(src_folder):
-                file = open(f"{self.path()}/{split}/{annotation_file.split('.')[0]}.txt","w")
-                annotation:list = json.load(open(f"{src_folder}/{annotation_file}", "r"))["form"]
+                file_path = os.path.join(dst_folder, annotation_file.replace("json", "txt"))
+                file = open(file_path,"w")
+                annotation:list = json.load(os.path.join(src_folder, annotation_file), "r")["form"]
                 
                 file_content = []
                 for label in annotation:
