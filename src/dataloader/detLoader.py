6e915522cb0c6c5a60a60ea5d444c6d85788ea6f
xReniar <ilaorainer@gmail.com>
2025-03-13 00:47:53 +0100

    Remove label_info and __loader__ functions

--- src/dataloader/detLoader.py
+++ src/dataloader/detLoader.py
@@ -1,6 +1,6 @@
 from .dataloader import Dataloader
+from ..utils import reader
 import multiprocessing
-import ast
 import os
 
 class DetDataloader(Dataloader):
@@ -24,28 +24,10 @@ class DetDataloader(Dataloader):
             for path in root_paths:
                 full_paths += [os.path.join(path, fn) for fn in os.listdir(path)]
 
-            self.data[split] = self.__loader__(full_paths)
+            pool = multiprocessing.Pool(processes = os.cpu_count())
+            labels = pool.map(reader.read_label, full_paths)
 
-    def __loader__(
-        self,
-        label_dir_paths: str 
-    ) -> list[tuple[str, list[tuple[str, list[int]]]]]:
-        pool = multiprocessing.Pool(processes=os.cpu_count())
-        labels = pool.map(self.label_info, label_dir_paths)
+            pool.close()
+            pool.join()
 
-        pool.close()
-        pool.join()
-
-        return labels
-    
-    @staticmethod
-    def label_info(
-        label_path: str
-    ) -> tuple[str, list[tuple[str, list[int]]]]:
-        with open(label_path, "r") as label_file:
-            label_content = []
-            rows = list(map(lambda row: row.strip("\n").split("\t"), label_file.readlines()))
-            for (text, bbox) in rows:
-                label_content.append((text, ast.literal_eval(bbox)))
-        
-            return (label_path.strip(".txt").replace("labels", "images"), label_content)
+            self.data[split] = labels
