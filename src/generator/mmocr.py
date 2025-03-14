4c18928f4fdbec78a3cafaaaa38be6e1d78cf2b4
xReniar <ilaorainer@gmail.com>
2025-03-15 00:09:32 +0100

    Update det and rec generation process

--- src/generator/mmocr.py
+++ src/generator/mmocr.py
@@ -1,7 +1,8 @@
 from .generator import Generator
-from ..dataloader.detLoader import DetDataloader
-from ..dataloader.recLoader import RecDataloader
-from PIL import Image
+from ..dataloader import Dataloader
+from pathlib import Path
+import multiprocessing
+import cv2
 import os
 import json
 
@@ -19,95 +20,92 @@ class MMOCRGenerator(Generator):
             transforms
         )
 
-    def _generate_det_data(self, dataloader: DetDataloader):
-        root_path = os.path.join(self.root_path, "Detection")
+    def _generate(
+        self,
+        dataloader: Dataloader,
+        task: str,
+        process,
+    ) -> None:
+        root_path = os.path.join(self.root_path, task)
         os.makedirs(root_path, exist_ok=True)
 
-        img_folder_name = "imgs"
+        for split in ["train", "test"]:
+            img_output_path = os.path.join(root_path, "imgs", split)
+            os.makedirs(img_output_path, exist_ok=True)
 
-        for split in ["train","test"]:
-            img_folder_path = f"{root_path}/{img_folder_name}/{split}"
-            os.makedirs(img_folder_path,exist_ok=True)
+            args = [(img_output_path, img_path, gt) for (img_path, gt) in dataloader.data[split]]
 
-            data_list = []
-            for dataset in self.datasets:
-                current_path = f"data/{dataset}"
+            with multiprocessing.Pool(processes=os.cpu_count()) as pool:
+                results = pool.starmap(process, args)
 
-                labels_dir = sorted(os.listdir(f"{current_path}/{split}"))
+            with open(os.path.join(root_path, f"{split}.json"), "w") as file:
+                labels = {}
+                if task == "Detection":
+                    labels = dict(
+                        metainfo = dict(
+                            dataset_type = "TextDetDataset",
+                            task_name = "textdet",
+                            category = [dict(id = 0,name = "text")]
+                        ),
+                        data_list = results
+                    )
+                if task == "Recognition":
+                    results = [item for sublist in results for item in sublist]
+                    labels = dict(
+                        metainfo = dict(
+                            dataset_type = "TextRecogDataset",
+                            task_name = "textrecog"
+                        ),
+                        data_list = results
+                    )
+                json.dump(labels, file, indent=4, ensure_ascii=False)
                 
-                dst_path = f"{root_path}/{img_folder_name}/{split}"
-                self.copy_file(
-                    labels_dir,
-                    current_path,
-                    dst_path
-                )
+    def _det(
+        self,
+        img_output_path: str,
+        img_path: str,
+        gt: list
+    ) -> dict:
+        img = cv2.imread(img_path)
+        _, img_name = os.path.split(img_path)
+        cv2.imwrite(os.path.join(img_output_path, img_name), img)
 
-                current_data_list = []
-                for label in labels_dir:
-                    img_fn = label.replace(".txt","")
-                    img = Image.open(f"{current_path}/images/{img_fn}")
-                    instances = []
-                    for (_, bbox) in self.read_rows(f"{current_path}/{split}/{label}"):
-                        x1, y1, x2, y2 = list(map(lambda point: float(point), bbox))
-                        instances.append(dict(
-                            polygon = [x1, y1, x2, y1, x2, y2, x1, y2],
-                            bbox = [x1, y1, x2, y2],
-                            bbox_label = 0,
-                            ignore = False
-                        ))
-                    current_data_list.append(dict(
-                        instances = instances,
-                        img_path = f"{img_folder_name}/{split}/{img_fn}",
-                        width = img.width,
-                        height = img.height
-                    ))
-                data_list.append(current_data_list)
+        split = Path(img_output_path).parts[-1]
+        instances = []
+        for (_, bbox) in gt:
+            x1, y1, x2, y2 = list(map(lambda point: float(point), bbox))
+            instances.append(dict(
+                polygon = [x1, y1, x2, y1, x2, y2, x1, y2],
+                bbox = [x1, y1, x2, y2],
+                bbox_label = 0,
+                ignore = False
+            ))
+        
+        return dict(
+            instances = instances,
+            img_path = os.path.join("imgs", split, img_name),
+            width = img.shape[1],
+            height = img.shape[0]
+        )
 
-            label = dict(
-                metainfo = dict(
-                    dataset_type = "TextDetDataset",
-                    task_name = "textdet",
-                    category = [dict(id = 0,name = "text")]
-                ),
-                data_list = data_list
-            )
+    def _rec(
+        self,
+        img_output_path: str,
+        img_path: str,
+        gt: list
+    ) -> list[dict]:
+        img = cv2.imread(img_path)
+        _, img_name = os.path.split(img_path)
+        split = Path(img_output_path).parts[-1]
+        data_list = []
+        for i, (text, bbox) in enumerate(gt):
+            crop = img[bbox[1]:bbox[3], bbox[0]:bbox[2]]
+            crop_name = img_name.replace(".", f"-{i}.")
+            cv2.imwrite(os.path.join(img_output_path, crop_name), crop)
 
-            with open(f"{root_path}/{split}.json", "w") as file:
-                json.dump(label, file, indent=4)
-
-    def _generate_rec_data(self, dataloader: RecDataloader):
-        root_path = os.path.join(self.root_path, "Recognition")
-        os.makedirs(root_path, exist_ok=True)
-        img_folder_name = "imgs"
-
-        for split in ["train","test"]:
-            img_folder_path = f"{root_path}/{img_folder_name}/{split}"
-            os.makedirs(img_folder_path,exist_ok=True)
-
-            data_list = []
-            for dataset in self.datasets:
-                current_path = f"data/{dataset}"
-
-                labels_dir = sorted(os.listdir(f"{current_path}/{split}"))
-
-                for label in labels_dir:
-                    img_fn = label.replace(".txt","")
-                    img = Image.open(f"{current_path}/images/{img_fn}")
-                    for index, (text, bbox) in enumerate(self.read_rows(f"{current_path}/{split}/{label}")):
-                        crop_name = img_fn.replace(".",f"-{index}.")
-                        img.crop(bbox).save(f"{root_path}/{img_folder_name}/{split}/{crop_name}")
-                        data_list.append(dict(
-                            instances = [{"text": text}],
-                            img_path = f"{img_folder_name}/{split}/{crop_name}"
-                        ))
-                    img.close()
-
-            label = dict(
-                metainfo = dict(
-                    dataset_type = "TextRecogDataset",
-                    task_name = "textrecog"
-                ),
-                data_list = data_list
-            )
-            with open(f"{root_path}/{split}.json", "w") as file:
-                json.dump(label, file, indent=4, ensure_ascii=False)
+            data_list.append(dict(
+                instances = [{"text": text}],
+                img_path = os.path.join("imgs", split, img_name)
+            ))
+        
+        return data_list
