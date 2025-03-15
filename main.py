16a9dc7612e2660a65a537e6dd41cd15894a81d5
xReniar <ilaorainer@gmail.com>
2025-03-16 00:05:38 +0100

    Update error checking and debug messages

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from src.dataset import DATASETS, CONFIG
 from src.generator import *
 from src.utils.draw import draw_labels
+from src.utils. dataset import check_images, check_labels
+import argparse
+import os
 import json
 import yaml
-import argparse
 
 
-def main(
+def pipeline(
     test_name: str,
     ocr_system: str,
     tasks: dict,
@@ -14,7 +16,8 @@ def main(
     augmentation_config,
     args
 ) -> None:
-
+    print("Downloading selected datasets")
+    
     # create dataset objects
     for dataset in datasets.keys():
         if "-" in dataset:
@@ -26,36 +29,49 @@ def main(
             datasets[dataset] = DATASETS[dataset.upper()](CONFIG[dataset.upper()])
         
         # download if necessary
-        dataset_instance = datasets[dataset]      
-        if not(dataset_instance.is_downloaded()):
-            print(f"Downloading {dataset_instance.path()}")
-            dataset_instance.download()
-            print(f"Downloaded {dataset_instance.path()}")
+        dataset_instance = datasets[dataset]    
+        dataset_instance.download()
 
-    # check if all the labels have a corresponding image
+    # check if all the labels have a corresponding image and viceversa
     # also check if there are wrong bounding boxes
+    if os.path.isfile("./errors.json"):
+        os.remove("./errors.json")
+
+    print("Checking errors")
     errors = {}
     for dataset in datasets.keys():
         dataset_instance = datasets[dataset]
+        missing = check_images(dataset_instance.path())
+        missing_images = missing["missing_images"]
+        missing_labels = missing["missing_labels"]
 
-    if args.draw:
-        for dataset in datasets.keys():
-            dataset_instance = datasets[dataset]
-            draw_labels(dataset_instance.path())
-            
-    
-    if args.generate:
-        if not(len(list(errors.keys())) != 0):
+        label_errors = check_labels(dataset_instance.path())
+
+        if not(len(missing_images) == 0 and len(missing_labels) == 0 and len(label_errors.keys()) == 0):
+            errors[dataset_instance.__str__()] = dict(
+                missing_images = missing["missing_images"],
+                missing_labels = missing["missing_labels"],
+                label_checking = label_errors
+            )
+
+    if len(list(errors.keys())) != 0:
+        with open("errors.json", "w") as error_file:
+            json.dump(errors, error_file, indent=4, ensure_ascii=False)
+        print("  [✗] Some bbox values are wrong, or some images or labels are missing. Details in `./errors.json`")
+    else:
+        print("  [✓] No errors in the selected datasets")
+
+        if args.draw:
+            for dataset in datasets.keys():
+                dataset_instance = datasets[dataset]
+                draw_labels(dataset_instance.path())
+        if args.generate:
             ocr_generator: Generator = OCR_SYSTEMS[ocr_system](
                 test_name,
                 list(datasets.keys()),
                 augmentation_config
             )
             ocr_generator.generate_data(tasks)
-        else:
-            with open("errors.json", "w") as error_file:
-                json.dump(errors, error_file, indent=4, ensure_ascii=False)
-            print("Some bbox values are wrong, details in `./errors.json`. Correct them before generating data")
 
 def parse_args():
     parser = argparse.ArgumentParser(
@@ -82,10 +98,11 @@ if __name__ == "__main__":
     datasets: dict = datasets_config["datasets"]
     defined_classes = list(map(lambda x: str(x).lower(), list(DATASETS.keys())))
     config_classes = list(datasets.keys())
+
     if len(defined_classes) > len(config_classes):
-        raise BaseException("There are some datasets that are not in the `config.json`")
+        print("  [✗] There are some datasets that are not defined in the `pipeline.yaml`")
     if len(defined_classes) < len(config_classes):
-        raise BaseException("Some datasets in the `config.json` do not have a script in `./src/dataset/`")
+        print("  [✗] Some datasets in the `config.json` do not have a script in `./src/dataset/`")
 
     selected_datasets = {}
     for dataset in config_classes:
@@ -101,11 +118,11 @@ if __name__ == "__main__":
             raise ValueError(f"Type '{type(value)}' not available")
 
     if not(any(value == "y" for value in TASKS.values())):
-        raise BaseException("Select at least one task. All the tasks are set to 'n'")
+        print("  [✗] Select at least one task. All the tasks are set to 'n'")
     
     args = parse_args()
 
-    main(
+    pipeline(
         TEST_NAME,
         OCR_SYSTEM,
         TASKS,
