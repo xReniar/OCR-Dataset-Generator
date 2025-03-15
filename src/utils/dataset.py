9ead54d840790945a7837fff11e1956cf1305701
xReniar <ilaorainer@gmail.com>
2025-03-16 00:10:16 +0100

    Update check_images and check_labels function

--- src/utils/dataset.py
+++ src/utils/dataset.py
@@ -1,12 +1,11 @@
 from .reader import read_labels
 import os
-import ast
 
 
 def check_images(
     dataset_path:str,
-    errors: dict
-) -> None:
+) -> dict:
+    errors = {}
     for split in ["train", "test"]:
         images_dir = set(map(lambda fn: fn.split(".")[0], sorted(os.listdir(os.path.join(dataset_path, split, "images")))))
         labels_dir = set(map(lambda fn: fn.split(".")[0], sorted(os.listdir(os.path.join(dataset_path, split, "labels")))))
@@ -18,21 +17,24 @@ def check_images(
         missing_images = labels_dir - images_dir
         if not(missing_images):
             errors["missing_images"] = list(missing_images)
+    
+    return errors
 
 def check_labels(
     dataset_path: str,
-    errors: dict
 ) -> None:
+    errors = {}
     for split in ["train", "test"]:
-        label_dir = sorted(os.listdir(os.path.join(dataset_path, split, "labels")))
+        label_dir_path = os.path.join(dataset_path, split, "labels")
 
-        labels_content = read_labels(label_dir)
+        labels_content = read_labels(label_dir_path)
         for label_filename in labels_content.keys():
             for i, (text, bbox) in enumerate(labels_content[label_filename]):
-                x1, y1, x2, y2 = tuple(ast.literal_eval(bbox))
+                x1, y1, x2, y2 = bbox
                 if not(x1 < x2 and y1 < y2):
                     errors[os.path.join(dataset_path, split, "labels", label_filename)] = dict(
                         line = i + 1,
                         text = text,
                         bbox = [x1, y1, x2, y2]
                     )
+    return errors
