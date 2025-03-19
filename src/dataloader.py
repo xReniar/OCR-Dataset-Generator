31223555e632acbde1f32c826e7fadbc195a668d
xReniar <ilaorainer@gmail.com>
2025-03-19 09:49:35 +0100

    Implement filter function with lang variable

--- src/dataloader.py
+++ src/dataloader.py
@@ -7,13 +7,13 @@ import os
 class Dataloader(ABC):
     def __init__(
         self,
-        transforms: dict,
-        datasets: list[str]
+        datasets: list[str],
+        lang: list[str] | None
     ) -> None:
         super().__init__()
 
-        self._transforms = transforms
         self._datasets = datasets
+        self._lang = set(lang) if lang != None else lang
         self.data = dict(
             train = [],
             test = []
@@ -31,12 +31,21 @@ class Dataloader(ABC):
                 full_paths += [os.path.join(path, fn) for fn in os.listdir(path)]
 
             with multiprocessing.Pool(processes=os.cpu_count()) as pool:
-                labels = pool.map(reader.read_label, full_paths)
-
-            self.data[split] = labels
+                self.data[split] = self.__filter__(pool.map(reader.read_label, full_paths))
     
     def __filter__(
         self,
-        data: list
-    ) -> None:
-        pass
+        labels: list[tuple[str, list[tuple[str, list[int]]]]]
+    ) -> list[tuple[str, list[tuple[str, list[int]]]]]:
+        if self._lang != None:
+            new_labels = []
+            for (img_name, img_labels) in labels:
+                filtered_labels = []
+                for (text, bbox) in img_labels:
+                    if all(char in self._lang for char in text):
+                        filtered_labels.append((text, bbox))
+                if len(filtered_labels) != 0:
+                    new_labels.append((img_name, filtered_labels))
+            return new_labels
+        else:
+            return labels
