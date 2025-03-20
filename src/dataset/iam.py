0805b72c79fcf695e0468da1d00ae499fb033bc5
xReniar <ilaorainer@gmail.com>
2025-03-20 20:07:55 +0100

    Add link for IAM dataset

--- src/dataset/iam.py
+++ src/dataset/iam.py
@@ -1,9 +1,12 @@
 from .dataset import Dataset
 
-CONFIG = {
-    "iam": "naderabdalghani/iam-handwritten-forms-dataset"
-}
 
+CONFIG = {
+    "iam": [
+        "naderabdalghani/iam-handwritten-forms-dataset",
+        "nibinv23/iam-handwriting-word-database"
+    ]
+}
 
 class IAM(Dataset):
     def __init__(
