5e074959aace3044a8dd5b9d573f37132c18348f
xReniar <ilaorainer@gmail.com>
2025-03-21 11:29:02 +0100

    Add IAM images downloader

--- src/dataset/iam.py
+++ src/dataset/iam.py
@@ -1,11 +1,15 @@
 from .dataset import Dataset
+import requests
+import zipfile
+import os
+import shutil
 
 
 CONFIG = {
-    "iam": [
-        "naderabdalghani/iam-handwritten-forms-dataset",
-        "nibinv23/iam-handwriting-word-database"
-    ]
+    "iam": {
+        "images-link": "https://www.kaggle.com/api/v1/datasets/download/naderabdalghani/iam-handwritten-forms-dataset",
+        "labels-link": "nibinv23/iam-handwriting-word-database"
+    }
 }
 
 class IAM(Dataset):
@@ -16,4 +20,30 @@ class IAM(Dataset):
         super().__init__(config)
 
     def _download(self):
-        pass
+        zip_path = os.path.join(self.path(), "dataset.zip")
+        with requests.get(
+            self.config[self._current]["images-link"],
+            allow_redirects=True,
+            stream=True
+        ) as response:
+            response.raise_for_status()
+            with open(zip_path, "wb") as f:
+                for chunk in response.iter_content(chunk_size=8192 * 4):
+                    if chunk:
+                        f.write(chunk)
+
+        with zipfile.ZipFile(zip_path, "r") as zip_ref:
+            zip_ref.extractall(self.path())
+
+        os.remove(os.path.join(self.path(), "dataset.zip"))
+        os.remove(os.path.join(self.path(), "__notebook_source__.ipynb"))
+
+        data_folder_path = os.path.join(self.path(), "data")
+        for folder in os.listdir(data_folder_path):
+            folder_path = os.path.join(self.path(), "data", folder)
+            for fn in os.listdir(folder_path):
+                shutil.move(
+                    os.path.join(folder_path, fn),
+                    os.path.join(data_folder_path)
+                )
+            os.removedirs(folder_path)
