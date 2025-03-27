7c77b2553eb48db0fe256484341e4e1ec6117660
xReniar <ilaorainer@gmail.com>
2025-03-27 14:34:04 +0100

    Add easyocr generator script

---
+++ src/generator/easyocr.py
@@ -0,0 +1,42 @@
+from .generator import Generator
+from ..dataloader import Dataloader
+
+
+class EasyOCRGenerator(Generator):
+    def __init__(
+        self,
+        test_name: str,
+        datasets: list, 
+        lang: list[str] | None,
+        transforms = None
+    ) -> None:
+        super().__init__(
+            test_name,
+            datasets,
+            lang,
+            transforms
+        )
+
+    def _generate(
+        self,
+        dataloader: Dataloader,
+        task: str,
+        process
+    ) -> None:
+        pass
+
+    def _det(
+        self,
+        img_output_path: str,
+        img_path: str,
+        gt: list
+    ) -> None:
+        pass
+
+    def _rec(
+        self,
+        img_output_path: str,
+        img_path: str,
+        gt: list
+    ) -> None:
+        pass
