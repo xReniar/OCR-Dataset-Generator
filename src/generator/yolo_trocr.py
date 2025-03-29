76a51f873b1bda7e579f8c5715db13b584015745
xReniar <ilaorainer@gmail.com>
2025-03-30 00:39:14 +0100

    Add detection and recognition data generation

--- src/generator/yolo_trocr.py
+++ src/generator/yolo_trocr.py
@@ -1,5 +1,8 @@
 from .generator import Generator
 from ..dataloader import Dataloader
+from PIL import Image
+import multiprocessing
+import os
 
 
 class YOLOTrOCRGenerator(Generator):
@@ -23,7 +26,25 @@ class YOLOTrOCRGenerator(Generator):
         task: str,
         process
     ) -> None:
-        pass
+        root_path = os.path.join(self.root_path, task)
+        os.makedirs(root_path, exist_ok=True)
+
+        for split in ["train", "test"]:
+            img_output_path = os.path.join(root_path, split)
+            os.makedirs(img_output_path, exist_ok=True)
+            if task == "Detection":
+                os.makedirs(os.path.join(img_output_path, "images"), exist_ok=True)
+                os.makedirs(os.path.join(img_output_path, "labels"), exist_ok=True)
+
+            args = [(img_output_path, img_path, gt) for (img_path, gt) in dataloader.data[split]]
+
+            with multiprocessing.Pool(processes=os.cpu_count()) as pool:
+                results = pool.starmap(process, args)
+
+            if task == "Recognition":
+                with open(os.path.join(root_path, f"{split}.txt"), "w") as file:
+                    results = [item for sublist in results for item in sublist]
+                    file.writelines(results)
 
     def _det(
         self,
@@ -31,12 +52,46 @@ class YOLOTrOCRGenerator(Generator):
         img_path: str,
         gt: list
     ) -> None:
-        pass
+        img = Image.open(img_path)
+        if img.mode == 'RGBA':
+            img = img.convert('RGB')
+
+        _, img_name = os.path.split(img_path)
+        img_id = img_name.split(".")[0]
+        img.save(os.path.join(img_output_path, "images", img_name))
+        width, height = img.width, img.height
+
+        with open(os.path.join(img_output_path, "labels", f"{img_id}.txt"), "w") as file:
+            for (_, bbox) in gt:
+                x1, y1, x2, y2 = bbox
+                x_center = (x1 + x2 / 2) / width
+                y_center = (y1 + y2 / 2) / height
+                box_width = (x2 - x1) / width
+                box_height = (y2 - y1) / height
+
+                # every text has label 0 (for now)
+                # "label_id = func(text)"" or the text is the label itself
+                file.write(f"0 {x_center} {y_center} {box_width} {box_height}\n")
+        img.close()
 
     def _rec(
         self,
         img_output_path: str,
         img_path: str,
         gt: list
-    ) -> None:
-        pass
+    ) -> list[str]:
+        img = Image.open(img_path)
+        if img.mode == 'RGBA':
+            img = img.convert('RGB')
+
+        _, img_name = os.path.split(img_path)
+        result = []
+        for i, (text, bbox) in enumerate(gt):
+            crop = img.crop(bbox)
+            crop_name = img_name.replace(".", f"-{i}.")
+            crop.save(os.path.join(img_output_path, crop_name))
+            crop.close()
+
+            result.append(f"{crop_name} {text}\n")
+        
+        return result
