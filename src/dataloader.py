c3d9f4bbd722905001bcc4196d86da9a1f63a1d8
xReniar <ilaorainer@gmail.com>
2025-04-18 01:57:31 +0200

    Rename lang to dict

--- src/dataloader.py
+++ src/dataloader.py
@@ -8,21 +8,18 @@ class Dataloader(ABC):
     def __init__(
         self,
         datasets: list[str],
-        lang: list[str] | None
+        dict: list[str] | None
     ) -> None:
         super().__init__()
 
         self._datasets = datasets
-        self._lang = set(lang) if lang != None else lang
-        self.data = dict(
-            train = [],
-            test = []
-        )
+        self._dict = set(dict) if dict != None else dict
+        self.data = { "train": [],"test": [] }
         self._workers = os.cpu_count()
         
-        self.__load_data__()
+        self.load_data()
 
-    def __load_data__(self) -> None:
+    def load_data(self) -> None:
         for split in ["train", "test"]:
             root_paths = [os.path.join("data", dataset, split, "labels") for dataset in self._datasets]
 
@@ -31,18 +28,18 @@ class Dataloader(ABC):
                 full_paths += [os.path.join(path, fn) for fn in os.listdir(path)]
 
             with multiprocessing.Pool(processes=self._workers) as pool:
-                self.data[split] = self.__filter__(pool.map(reader.read_label, full_paths))
+                self.data[split] = self.filter(pool.map(reader.read_label, full_paths))
     
-    def __filter__(
+    def filter(
         self,
         labels: list[tuple[str, list[tuple[str, list[int]]]]]
     ) -> list[tuple[str, list[tuple[str, list[int]]]]]:
-        if self._lang != None:
+        if self._dict != None:
             new_labels = []
             for (img_name, img_labels) in labels:
                 filtered_labels = []
                 for (text, bbox) in img_labels:
-                    if all(char in self._lang for char in text):
+                    if all(char in self._dict for char in text):
                         filtered_labels.append((text, bbox))
                 if len(filtered_labels) != 0:
                     new_labels.append((img_name, filtered_labels))
