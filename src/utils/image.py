0fbafcfb5f9c3e2f46f1f1afe225bb6c5be49e88
xReniar <ilaorainer@gmail.com>
2025-04-18 23:17:36 +0200

    Fix return type

--- src/utils/image.py
+++ src/utils/image.py
@@ -1,22 +1,22 @@
 from PIL import Image
+import numpy as np
+import cv2
 
 
 def open_image(
-    img_path: str,
-    mode: str = "RGB"
-) -> Image.ImageFile:
+    img_path: str
+) -> Image.Image:
     """
-    Open an image and convert it to the specified mode.
+    Open an image file and convert it to a format suitable for processing.
 
     Args:
         img_path (str): Path to the image file.
-        mode (str): Mode to convert the image to. Default is "RGB".
 
     Returns:
         Image.Image: The opened and converted image.
     """
     img = Image.open(img_path)
-    if img.mode in ['RGBA', 'LA']:
-        img = img.convert(mode)
+    if img.mode in ["RGBA", "LA"]:
+        img = img.convert("RGB")
         
     return img
