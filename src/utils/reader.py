8d4f744a78240ebf689e93af312ad519ec65f02c
xReniar <ilaorainer@gmail.com>
2025-04-18 19:19:09 +0200

    Add comment

--- src/utils/reader.py
+++ src/utils/reader.py
@@ -5,6 +5,15 @@ import ast
 def read_label(
     label_path:str
 ) -> tuple[str, list[tuple[str, list[int]]]]:
+    """
+    Reads the label file and returns the image path and the labels.
+
+    Args:
+        label_path (str): Path to the label file.
+
+    Returns:
+        tuple[str, list[tuple[str, list[int]]]]: A tuple containing the image path and a list of tuples with text and bounding box coordinates.
+    """
     label_content = []
     with open(label_path, "r") as label_file:
         rows = list(map(lambda row: row.strip("\n").split("\t"), label_file.readlines()))
@@ -16,6 +25,15 @@ def read_label(
 def read_labels(
     label_dir_path:str
 ) -> dict[str, list[tuple[str, list[int]]]]:
+    """
+    Reads all label files in a directory and returns a dictionary with image paths as keys and label contents as values.
+
+    Args:
+        label_dir_path (str): Path to the directory containing label files.
+
+    Returns:
+        dict[str, list[tuple[str, list[int]]]]: A dictionary with image paths as keys and label contents as values.
+    """
     labels_content = {}
     for label_filename in os.listdir(label_dir_path):
         _, label_content = read_label(os.path.join(label_dir_path, label_filename))
