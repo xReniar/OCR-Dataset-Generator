daaeb1cb3e53bfe5a3db9dc91a5f41ae2a0000f5
xReniar <ilaorainer@gmail.com>
2025-04-19 01:18:17 +0200

    Fix open image process

--- src/utils/image.py
+++ src/utils/image.py
@@ -4,8 +4,9 @@ import cv2
 
 
 def open_image(
-    img_path: str
-) -> Image.Image:
+    img_path: str,
+    transform: callable = None
+) -> np.ndarray:
     """
     Open an image file and convert it to a format suitable for processing.
 
@@ -13,10 +14,16 @@ def open_image(
         img_path (str): Path to the image file.
 
     Returns:
-        Image.Image: The opened and converted image.
+        cv_image (numpy.ndarray): Image in OpenCV format.
     """
     img = Image.open(img_path)
     if img.mode in ["RGBA", "LA"]:
         img = img.convert("RGB")
+
+    cv_image = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+    img.close()
+
+    if transform is not None:
+        cv_image = transform(image=cv_image)["image"]
         
-    return img
+    return cv_image
