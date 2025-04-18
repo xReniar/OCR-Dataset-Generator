4b844249a3b3b47332fa39e829400635934d5e45
xReniar <ilaorainer@gmail.com>
2025-04-18 23:17:10 +0200

    Modify get_operations function

--- src/augmenter.py
+++ src/augmenter.py
@@ -3,31 +3,13 @@ import albumentations as A
 
 class DataAugmenter():
     def __init__(
-        self,
-        config: dict,
+        self
     ) -> None:
-        pass
+        self.transforms = None
 
-    def augment(self) -> list:
-        return [
-            A.Compose([
-                A.RandomCrop(width=512, height=512),
-                A.HorizontalFlip(p=0.5),
-                A.VerticalFlip(p=0.5),
-                A.Rotate(limit=90, p=0.5),
-                A.RandomBrightnessContrast(p=0.5),
-                A.GaussNoise(p=0.5),
-                A.Blur(blur_limit=(3, 7), p=0.5),
-                A.HueSaturationValue(p=0.5),
-            ]),
-            A.Compose([
-                A.RandomCrop(width=512, height=512),
-                A.HorizontalFlip(p=0.5),
-                A.VerticalFlip(p=0.5),
-                A.Rotate(limit=90, p=0.5),
-                A.RandomBrightnessContrast(p=0.5),
-                A.GaussNoise(p=0.5),
-                A.Blur(blur_limit=(3, 7), p=0.5),
-                A.HueSaturationValue(p=0.5),
-            ])
-        ]
+    def get_operations(self) -> list:
+        self.transforms = {
+            "blur": A.Blur(blur_limit=7),
+            "sharpen": A.Sharpen(alpha=0.5),
+            "brightness": A.RandomBrightnessContrast(brightness_limit=0.2),
+        }
