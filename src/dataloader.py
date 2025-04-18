e9aee538ac30af4caf05cea07df1503210a2de48
xReniar <ilaorainer@gmail.com>
2025-04-18 02:03:35 +0200

    Turn variable from protected to public

--- src/dataloader.py
+++ src/dataloader.py
@@ -12,34 +12,34 @@ class Dataloader(ABC):
     ) -> None:
         super().__init__()
 
-        self._datasets = datasets
-        self._dict = set(dict) if dict != None else dict
+        self.datasets = datasets
+        self.dict = set(dict) if dict != None else dict
         self.data = { "train": [],"test": [] }
-        self._workers = os.cpu_count()
+        self.workers = os.cpu_count()
         
         self.load_data()
 
     def load_data(self) -> None:
         for split in ["train", "test"]:
-            root_paths = [os.path.join("data", dataset, split, "labels") for dataset in self._datasets]
+            root_paths = [os.path.join("data", dataset, split, "labels") for dataset in self.datasets]
 
             full_paths = []
             for path in root_paths:
                 full_paths += [os.path.join(path, fn) for fn in os.listdir(path)]
 
-            with multiprocessing.Pool(processes=self._workers) as pool:
+            with multiprocessing.Pool(processes=self.workers) as pool:
                 self.data[split] = self.filter(pool.map(reader.read_label, full_paths))
     
     def filter(
         self,
         labels: list[tuple[str, list[tuple[str, list[int]]]]]
     ) -> list[tuple[str, list[tuple[str, list[int]]]]]:
-        if self._dict != None:
+        if self.dict != None:
             new_labels = []
             for (img_name, img_labels) in labels:
                 filtered_labels = []
                 for (text, bbox) in img_labels:
-                    if all(char in self._dict for char in text):
+                    if all(char in self.dict for char in text):
                         filtered_labels.append((text, bbox))
                 if len(filtered_labels) != 0:
                     new_labels.append((img_name, filtered_labels))
