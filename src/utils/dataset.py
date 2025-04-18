b1dcc60f52ff09cc8c09e0c5c1f82702b76a801c
xReniar <ilaorainer@gmail.com>
2025-04-18 19:20:28 +0200

    Add comment

--- src/utils/dataset.py
+++ src/utils/dataset.py
@@ -5,6 +5,18 @@ import os
 def check_images(
     dataset_path:str,
 ) -> dict:
+    """
+    Check if the images and labels are correct. The images and labels are correct if:
+    - The images have a corresponding label.
+    - The labels have a corresponding image.
+
+    Args:
+        dataset_path (str): Path to the dataset.
+        
+    Returns:
+        dict: Dictionary containing the errors. The keys are the paths to the images and labels and the values are
+        dictionaries containing the missing images and labels.
+    """
     errors = {}
     for split in ["train", "test"]:
         images_dir = set(map(lambda fn: fn.split(".")[0], sorted(os.listdir(os.path.join(dataset_path, split, "images")))))
@@ -26,7 +38,19 @@ def check_images(
 
 def check_labels(
     dataset_path: str,
-) -> None:
+) -> dict:
+    """
+    Check if the labels are correct. The labels are correct if:
+    - The bounding box coordinates are correct (x1 < x2 and y1 < y2).
+    - The text is not empty.
+
+    Args:
+        dataset_path (str): Path to the dataset.
+
+    Returns:
+        dict: Dictionary containing the errors. The keys are the paths to the labels and the values are dictionaries
+        containing the line number, text and bounding box coordinates.
+    """
     errors = {}
     for split in ["train", "test"]:
         label_dir_path = os.path.join(dataset_path, split, "labels")
