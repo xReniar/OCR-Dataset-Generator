d5e64242d5f7dc9769183b271e33bd4c9d958c37
xReniar <ilaorainer@gmail.com>
2025-04-19 01:16:10 +0200

    Add augment operation

--- src/augmenter.py
+++ src/augmenter.py
@@ -9,7 +9,10 @@ class DataAugmenter():
 
     def get_operations(self) -> list:
         self.transforms = {
-            "blur": A.Blur(blur_limit=7),
-            "sharpen": A.Sharpen(alpha=0.5),
-            "brightness": A.RandomBrightnessContrast(brightness_limit=0.2),
+            "blur": A.Blur(
+                blur_limit=7,
+                p=1.0
+            )
         }
+
+        return list(self.transforms.items())
