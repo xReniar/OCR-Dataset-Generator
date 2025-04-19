9515f6e0b34105f43f1dac3ad328f1bd3775d4eb
xReniar <ilaorainer@gmail.com>
2025-04-19 16:35:52 +0200

    Handle cv2 rotation issue

--- src/utils/image.py
+++ src/utils/image.py
@@ -20,8 +20,18 @@ def open_image(
     if img.mode in ["RGBA", "LA"]:
         img = img.convert("RGB")
 
-    cv_image = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+    try:
+        deg = {3:180,6:270,8:90}.get(img.getexif().get(274,0),0)
+    except:
+        deg = 0
+
+    if deg != 0:
+        img = img.rotate(deg, expand=False)
+    cv_image = np.array(img)
+    if len(cv_image.shape)==3 and cv_image.shape[2] >= 3:
+        cv_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB) 
     img.close()
+    
     if transform is not None:
         cv_image = transform(image=cv_image)["image"]
     
