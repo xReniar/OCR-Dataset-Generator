0d265ce7adf4d33f9e7106c9d6b8cb798389163a
xReniar <ilaorainer@gmail.com>
2025-04-20 18:50:36 +0200

    Add easyocr dataset generation for detection task

--- src/generator/easyocr.py
+++ src/generator/easyocr.py
@@ -1,5 +1,9 @@
 from .generator import Generator
 from ..dataloader import Dataloader
+from ..utils.image import open_image
+import pandas as pd
+import cv2
+import os
 
 
 class EasyOCRGenerator(Generator):
@@ -24,7 +28,17 @@ class EasyOCRGenerator(Generator):
         dataloader: Dataloader,
         task: str
     ) -> None:
-        pass
+        root_path = os.path.join(self.root_path, task)
+        os.makedirs(root_path, exist_ok=True)
+
+        for split in ["train", "test"]:
+            if task == "Detection":
+                img_output_path = os.path.join(root_path, f"{split}ing_images")
+                label_output_path = os.path.join(root_path, f"{split}ing_localization_transcription_gt")
+                os.makedirs(img_output_path, exist_ok=True)
+                os.makedirs(label_output_path, exist_ok=True)
+
+                self.run_process(img_output_path, dataloader, task, split)
 
     def _det(
         self,
@@ -33,7 +47,21 @@ class EasyOCRGenerator(Generator):
         gt: list,
         transform: tuple[str, callable] = (None, None)
     ) -> None:
-        pass
+        img = open_image(img_path, transform[1])
+        _, img_name = os.path.split(img_path)
+        img_id, _ = tuple(img_name.split("."))
+
+        label_output_path = img_output_path.replace("images", "localization_transcription_gt")
+
+        if transform[0] is not None:
+            img_name = img_name.replace(".", f"-{transform[0]}.")
+
+        with open(os.path.join(label_output_path, f"{img_id}.txt"), "w") as file:
+            for (text, bbox) in gt:
+                x1, y1, x2, y2 = tuple(bbox)
+                file.write(f"{x1},{y1},{x2},{y1},{x2},{y2},{x1},{y2},{text}\n")
+
+        cv2.imwrite(os.path.join(img_output_path, img_name), img)
 
     def _rec(
         self,
@@ -42,4 +70,18 @@ class EasyOCRGenerator(Generator):
         gt: list,
         transform: tuple[str, callable] = (None, None)
     ) -> None:
-        pass
+        img = open_image(img_path, transform[1])
+        _, img_name = os.path.split(img_path)
+
+        result = []
+        for i, (text, bbox) in enumerate(gt):
+            crop = img[bbox[1]:bbox[3], bbox[0]:bbox[2]]
+            if transform[0] is not None:
+                crop_name = img_name.replace(".", f"-{i}-{transform[0]}.")
+            else:
+                crop_name = img_name.replace(".", f"-{i}.")   
+            cv2.imwrite(os.path.join(img_output_path, crop_name),crop)
+
+            result.append((crop_name, text))
+
+        return result
